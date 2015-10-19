606e762c1f89b9d5bacccabc47a339722e2e433f
Jean Rangel <jean_rangel94@hotmail.com>
2015-10-19 16:47:38 -0200

    Funcionando máquina local Jean Rangel (mysql com user configurado e caminhos dos relativos para imports de CSS e JS).

--- protected/view/headscripts.php
+++ protected/view/headscripts.php
@@ -2,25 +2,25 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 
 <!--begin file input-->
-<link href="/css/enhanced.css" rel="Stylesheet">
+<link href="css/enhanced.css" rel="Stylesheet">
 <!--End file input-->
 
 <!-- Le styles -->
-<link href="/assets/css/bootstrap.min.css" rel="stylesheet">
-<link href="/css/file-icons.css" rel="stylesheet">
-<link href="/css/custom-theme/jquery-ui-1.10.0.custom.css" rel="stylesheet" />
-<link href="/assets/css/font-awesome.min.css" rel="stylesheet" />
-<link href="/css/estilo.css?v=<?= $versao ?>" rel="stylesheet">
+<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+<link href="css/file-icons.css" rel="stylesheet">
+<link href="css/custom-theme/jquery-ui-1.10.0.custom.css" rel="stylesheet" />
+<link href="assets/css/font-awesome.min.css" rel="stylesheet" />
+<link href="css/estilo.css?v=<?= $versao ?>" rel="stylesheet">
 <!--[if IE 7]>
 <link rel="stylesheet" type="text/css" href="/assets/css/font-awesome-ie7.min.css">
 <![endif]-->
 <!--[if lt IE 9]>
 <link rel="stylesheet" type="text/css" href="/css/custom-theme/jquery.ui.1.10.0.ie.css"/>
 <![endif]-->
-<link href="/assets/js/google-code-prettify/prettify.css" rel="stylesheet">
+<link href="assets/js/google-code-prettify/prettify.css" rel="stylesheet">
 
 <!--begin cssMenu menu-->
-<link href="/css/nav.css" rel="stylesheet" type="text/css" />
+<link href="css/nav.css" rel="stylesheet" type="text/css" />
 <!--End cssMenu menu-->
 
 <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
@@ -29,8 +29,8 @@
 <![endif]-->
 
 <!-- Le fav and touch icons -->
-<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/ico/apple-touch-icon-144-precomposed.png">
-<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/assets/ico/apple-touch-icon-114-precomposed.png">
-<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/assets/ico/apple-touch-icon-72-precomposed.png">
-<link rel="apple-touch-icon-precomposed" href="/assets/ico/apple-touch-icon-57-precomposed.png">
-<link rel="shortcut icon" href="/assets/ico/favicon.png"><?php 
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="assets/ico/favicon.png"><?php 
